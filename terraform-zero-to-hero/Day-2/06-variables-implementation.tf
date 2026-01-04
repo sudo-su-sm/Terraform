1ef1a4ee344711826da69ab3698891057cf8799d
awslinux88 <awslinux88@gmailcom>
2026-01-04 12:59:15 +0000

    fixed the syntax issue

--- terraform-zero-to-hero/Day-2/06-variables-implementation.tf
+++ terraform-zero-to-hero/Day-2/06-variables-implementation.tf
@@ -1,6 +1,6 @@
 # Variables Demo
 
-```hcl
+
 
 # Define an input variable for the EC2 instance type
 variable "instance_type" {
@@ -32,4 +32,3 @@ output "public_ip" {
   value       = aws_instance.example_instance.public_ip
 }
 
-```
